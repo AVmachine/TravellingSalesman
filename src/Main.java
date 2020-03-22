7cdd78f1fa00b3a24229208dd1bbf22893431662
Alex A <alex.av936@gmail.com>
2020-03-21 21:44:38 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    public float pythagorean(Point A, Point B)
+    {
+        
+        return hypotenuse;
+    }
+
+}
---
+++ src/Point.java
@@ -0,0 +1,5 @@
+public class Point {
+    private char letter;
+    private int x;
+    private int y;
+}
